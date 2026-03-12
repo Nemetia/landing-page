501aacf7372e43f281d0d831975acafbcfe8a2e0
DANI-PC\dani- <dani-15-94@hotmail.com>
2026-03-12 17:24:55 +0100

    add nojekyll to avoid overwriting of styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nemetia.github.io',
-  base: '/landing-page'
+  base: '/landing-page/'
 });
